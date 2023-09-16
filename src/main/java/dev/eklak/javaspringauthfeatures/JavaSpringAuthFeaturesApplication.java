5025ed604acdb7bea9b9eac9ab3b511020dab63e
Eklak Dangaura <azens1995@gmail.com>
2023-09-16 13:40:04 +0545

    initialize project with base dependencies

---
+++ src/main/java/dev/eklak/javaspringauthfeatures/JavaSpringAuthFeaturesApplication.java
@@ -0,0 +1,12 @@
+package dev.eklak.javaspringauthfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAuthFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAuthFeaturesApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/eklak/javaspringauthfeatures/JavaSpringAuthFeaturesApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.eklak.javaspringauthfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAuthFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
